4fc2778852b11049a566e0a0594ee986a0ec2f5a
YoonSeongjune <yoonsj7tjdwns@gmail.com>
2026-01-21 09:58:41 +0000

    update src files

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Hello World!" << '\n'; // 같은 동작 end line을 수행
+    // 은 한 줄 주석
+
+    /*
+    이거는 여러줄 주석
+    */
+
     return 0;
 }
